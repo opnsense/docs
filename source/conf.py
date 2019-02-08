98101f1ac58e261755731045afbbdc07d241f852
Michael Steenbeek <42928941+MichaelDeciso@users.noreply.github.com>
2019-02-08 09:10:35 +0100

    Fix text wrapping in tables (#131)

--- source/conf.py
+++ source/conf.py
@@ -164,6 +164,12 @@ html_favicon = '_static/favicon.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [
+        '_static/css/theme-overrides.css',  # override wide tables in RTD theme
+    ]
+}
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
